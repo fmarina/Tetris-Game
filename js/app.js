af24f106dac40d30a7c72272658f44eeee2b4140
Marina <marinafaenze@hotmail.com>
2020-05-22 12:21:49 -0300

    tetris shapes created

--- js/app.js
+++ js/app.js
@@ -1,6 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     crearGrid(200);
+    const grid = document.querySelector("#grid");
+    let squares = Array.from(document.querySelectorAll("#grid div"));
+
+    // Shapes
+    const lShape = [
+        [1, 2, 11, 21],
+        [10, 11, 12, 22],
+        [1, 11, 20, 21],
+        [10, 20, 21, 22]
+    ];
+
+    const zShape = [
+        [11, 12, 20, 21],
+        [0, 10, 11, 21],
+        [10, 11, 21, 22],
+        [2, 11, 12, 21]
+    ]
+
+    const tShape = [
+        [1, 10, 11, 12],
+        [1, 11, 12, 21],
+        [1, 10, 11, 21],
+        [10, 11, 12, 21]
+    ];
+
+    const oShape = [
+        [0, 1, 10, 11],
+        [0, 1, 10, 11],
+        [0, 1, 10, 11],
+        [0, 1, 10, 11],
+    ];
+
+    const iShape = [
+        [1, 11, 21, 31],
+        [10, 11, 12, 13],
+        [1, 11, 21, 31],
+        [10, 11, 12, 13]
+    ]
+
+
 
 });
 
